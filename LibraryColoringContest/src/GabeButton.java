b0297ccd558ae8ed594cb059b4bcb0ebf8203e85
ghoule636 <ghoule636@gmail.com>
2016-02-02 16:26:14 -0800

    Added GabeButton

---
+++ LibraryColoringContest/src/GabeButton.java
@@ -0,0 +1,32 @@
+import javax.swing.Action;
+import javax.swing.Icon;
+import javax.swing.JButton;
+
+
+public class GabeButton extends JButton {
+
+	public GabeButton() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public GabeButton(Icon icon) {
+		super(icon);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GabeButton(String text) {
+		super(text);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GabeButton(Action a) {
+		super(a);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GabeButton(String text, Icon icon) {
+		super(text, icon);
+		// TODO Auto-generated constructor stub
+	}
+
+}
