d782bd68129194b65712807fd1192ce6079f9193
ghoule636 <ghoule636@gmail.com>
2016-02-04 19:02:33 -0800

    Displays a small panel with the Gabe button on it to test.

--- LibraryColoringContest/src/EasterEgg.java
+++ LibraryColoringContest/src/EasterEgg.java
@@ -1,8 +1,15 @@
+import javax.swing.JFrame;
+
 
 public class EasterEgg {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Go hawks!");
+		JFrame testFrame = new JFrame();
+		testFrame.add(new GabeButton());
+		testFrame.setSize(200, 200);
+		testFrame.setVisible(true);
+		
 	}
 }
