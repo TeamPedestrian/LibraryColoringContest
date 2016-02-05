b8ad22ad98742cdb0b4e7659ad081d910ce32939
ghoule636 <ghoule636@gmail.com>
2016-02-04 18:45:17 -0800

    Added gabe button.

--- LibraryColoringContest/src/GabeButton.java
+++ LibraryColoringContest/src/GabeButton.java
@@ -1,32 +1,30 @@
-import javax.swing.Action;
-import javax.swing.Icon;
+/*
+ * TCSS360 GabeButton
+ */
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 
 
+/**
+ * This class is a button that will activate an easter egg with information about
+ * Gabriel(me!).
+ * 
+ * @author Gabriel Houle
+ */
 public class GabeButton extends JButton {
 
+	private static final long serialVersionUID = 7711521263918528653L;
+
 	public GabeButton() {
-		System.out.println("Gabe_Button");
+		super("Gabriel Houle");
+		addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(final ActionEvent theEvent) {
+				JOptionPane.showMessageDialog(null, "I am cool.", "Gabe",JOptionPane.PLAIN_MESSAGE);
+			}
+		});
 	}
-
-	public GabeButton(Icon icon) {
-		super(icon);
-		// TODO Auto-generated constructor stub
-	}
-
-	public GabeButton(String text) {
-		super(text);
-		// TODO Auto-generated constructor stub
-	}
-
-	public GabeButton(Action a) {
-		super(a);
-		// TODO Auto-generated constructor stub
-	}
-
-	public GabeButton(String text, Icon icon) {
-		super(text, icon);
-		// TODO Auto-generated constructor stub
-	}
-
 }
