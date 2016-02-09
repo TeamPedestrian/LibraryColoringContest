6f2bb09eb523bc0ceac4a6ce5deffe2a6d1c3244
Antonio <Antonio@10.16.5.196>
2016-02-09 11:23:57 -0800

    Image added

--- LibraryColoringContest/src/AntonioButton.java
+++ LibraryColoringContest/src/AntonioButton.java
@@ -1,5 +1,24 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 
 public class AntonioButton extends JButton {
 
+	/***/
+	private static final long serialVersionUID = -3792865140287035679L;
+
+	/***/
+	public AntonioButton() {
+		super("Antonio");
+		addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(final ActionEvent theEvent) {
+				JOptionPane.showMessageDialog(null, "Antonio is a Senior at UWT majoring in CSS.\n"
+													+ "When not studying he likes to spend time with"
+													+ "his wife and their Chihuahua \"Tiki\"", "About",JOptionPane.PLAIN_MESSAGE);
+			}
+		});
+	}
 }
--- LibraryColoringContest/src/EasterEgg.java
+++ LibraryColoringContest/src/EasterEgg.java
@@ -1,15 +1,30 @@
+import java.awt.BorderLayout;
+import java.net.URL;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 
 public class EasterEgg {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		System.out.println("Go hawks!");
-		JFrame testFrame = new JFrame();
-		testFrame.add(new GabeButton());
-		testFrame.setSize(200, 200);
-		testFrame.setVisible(true);
+        URL image = EasterEgg.class.getResource("/resources/agile_method.png");
+        ImageIcon imageIcon = new ImageIcon(image);
+		JFrame easterEggFrame = new JFrame();
+		JPanel imagePanel = new JPanel();
+		imagePanel.add(new JLabel(imageIcon));
+		easterEggFrame.add(imagePanel);
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.add(new GabeButton());
+		buttonPanel.add(new AntonioButton());
+		easterEggFrame.add(buttonPanel, BorderLayout.SOUTH);
+		easterEggFrame.setSize(500, 500);
+		easterEggFrame.setLocationRelativeTo(null);
+		easterEggFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		easterEggFrame.setResizable(false);
+		easterEggFrame.setVisible(true);
 		
 	}
 }
