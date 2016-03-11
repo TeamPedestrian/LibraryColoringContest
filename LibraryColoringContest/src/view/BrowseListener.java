b7d1d5ffd3ef18f2035e3b5162836afc5ac73c0e
ghoule636 <ghoule636@gmail.com>
2016-03-10 16:01:10 -0800

    Merged Conflicts with andy and Yikalo's branches.

---
+++ LibraryColoringContest/src/view/BrowseListener.java
@@ -0,0 +1,71 @@
+package view;
+
+import java.awt.Menu;
+import java.awt.MenuBar;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JPanel;
+
+public class BrowseListener implements ActionListener {
+     JButton button ;
+        JLabel label;
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+
+        final JFrame frame = new JFrame("Please Select Age for Difficulty");
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(952, 500);
+        frame.setLocationRelativeTo(null);
+        //setupFrame(frame);
+        frame.setVisible(true);
+        
+        
+           JPanel center = new JPanel();
+           JMenuBar menuBar = new JMenuBar();
+          // setJMenuBar(menuBar);
+           
+           JMenu Menu = new JMenu("Design Selection");
+           menuBar.add(Menu);
+           frame.setJMenuBar(menuBar);
+           
+           
+                JButton button = new JButton("0 - 5");
+                //button.setBounds(300, 100, 100, 100);
+                center.add(button);
+                frame.add(center);
+                
+//                button.addActionListener(new ActionListner(){
+                    //public void ActionPerformed(ActionEvent e){
+                        
+                        
+                    //}
+//                });
+                    
+                
+                JButton button1 = new JButton("6 - 10");
+                center.add(button1);
+                frame.add(center);
+
+                JButton button2 = new JButton("11 - 15");
+                center.add(button2);
+                frame.add(center);
+                
+                JButton button3 = new JButton("16 - above");
+                center.add(button3);
+                frame.add(center);
+        
+    
+    
+    
+    }
+
+    
+
+}
